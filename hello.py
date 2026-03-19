30c80b87f347182e1c0a0d54efcafbac2626116c
johanlobo <johanloy16@gmail.com>
2026-03-19 12:12:37 +0530

    done

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
 print("hello")
 a = float(input("enter your USN:"))
 print("usn is:", a)
+m=1.21
+print (m*23423)
 
+print(1)
+z='loy'
+p='lobo'
+
+print('johan\t'*3)
+#34=n     #error!
+#print(n)
+#print(zp) error!
---
+++ price.py
@@ -0,0 +1,7 @@
+n=int(input("enter the number of copies:"))
+
+o=2328.67
+dp=2328.67*0.4
+op=o-dp
+z=(op+3)+(n-1)
+print("total cost of copies:\n",z,'rs')
