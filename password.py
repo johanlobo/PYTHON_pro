18d6c5f802bb47b2b1db00b816cc660bbe14cb1e
johanlobo <johanloy16@gmail.com>
2026-03-24 19:34:23 +0530

    done

--- password.py
+++ password.py
@@ -12,3 +12,28 @@ while True:
     else:
         print('passwords do not match, try again')
     #here if u enter loy as password and for RP if u give space and write loy it wont match cuz it will take space as a string character too
+    
+
+    #attempts = 0
+
+#while True:
+   # code = input("Please type in your PIN: ")
+   # attempts += 1
+
+   # if attempts == 3:
+    #    success = False
+     #   break
+
+   # if code == "1234":
+      #  success = True
+       # break
+
+   # print("Incorrect...try again")
+
+#if success:
+  #  print("Correct PIN entered!")
+#else:
+   # print("Too many attempts...")      USING RETURN 
+    
+
+
