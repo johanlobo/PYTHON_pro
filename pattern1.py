35ae58525c5d927688f523a97ec9e2f423eaf930
johanlobo <johanloy16@gmail.com>
2026-03-28 10:54:03 +0530

    done

---
+++ pattern1.py
@@ -0,0 +1,11 @@
+z=int(input('enter upper limit:'))
+i=1
+while i<=z:
+    a=1
+    while a<=z:
+        print(f"{i}x{a}={i*a}",end=' ')
+        a+=1
+        print()
+    i+=1
+
+    #understand the relation b/w patterns and nested loops.
