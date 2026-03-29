53563c1a7e14a71654399821bfe3e32691365a9b
johanlobo <johanloy16@gmail.com>
2026-03-29 15:38:59 +0530

    done

--- for.py
+++ for.py
@@ -10,7 +10,7 @@ for i in range (3,7):
     print(i)
 print()
 for i in range(1,10,2):
-    print(i)  #adds 2 to the previous number
+    print(i)  #adds 2 to the previous number. also u can write -ve integers to subtract ex:(7,-4,-3) output: 4, 1, -2
 print()
 z=int(input('please enter a positive integer:'))
 for i in range(-z,z+1):
