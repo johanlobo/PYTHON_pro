8d1f30705312e2c678804b098425ddddd4e4446d
johanlobo <johanloy16@gmail.com>
2026-03-29 11:06:12 +0530

    done

---
+++ pattern4.py
@@ -0,0 +1,15 @@
+def tria(pattern:str,rows:int):
+    i=0
+    while i<rows:
+        j=0
+        while j<=i:
+            print(pattern,end='')
+            j+=1
+        print()
+        i+=1
+
+tria('@',5)
+
+#recognize pattern
+
+    
